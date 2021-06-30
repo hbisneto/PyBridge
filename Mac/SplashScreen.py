3a6c78157eb4d6f1cda4d83235c535064767ffec
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2021-06-30 19:46:46 -0300

    PyBridge
    Updated "SplashScreen.py" on system modules

--- Mac/SplashScreen.py
+++ Mac/SplashScreen.py
@@ -4,20 +4,20 @@
 from datetime import date
 import getpass
 
-AnoAtual = date.today().year
+CurrentYear = date.today().year
 SoftwareName = "PyBridge"
-Version = "1.0"
-CopyrightName = getpass.getuser().capitalize()
+Version = "BETA 2"
+CopyrightName = "Heitor Bisneto (@Hbisneto)"
 
-print("Nome:", SoftwareName)
-print("Versão:", Version)
-print("Criado por:", CopyrightName)
+print("Name:", SoftwareName)
+print("Version:", Version)
+print("Created By:", CopyrightName)
 
-if AnoAtual == 2021:
-    print("Copyright ©", AnoAtual, "|", CopyrightName, "All rights reserved.")
+if CurrentYear == 2021:
+    print("Copyright ©", CurrentYear, "|", CopyrightName, "All rights reserved.")
 else:
-    print("Copyright © 2021 -", AnoAtual, "|", CopyrightName, "All rights reserved.")
+    print("Copyright © 2021 -", CurrentYear, "|", CopyrightName, "All rights reserved.")
 print()
 print("="*80)
-print(f'[{SoftwareName} for Mac] - Em Execução...')
+print(f'[{SoftwareName} for Mac] - Running...')
 print("="*80)
