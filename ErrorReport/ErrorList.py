ded21d43ac7ae14496cfcb329337dedeeb5e1693
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2021-07-06 06:13:34 -0300

    PyBridge
    PyBridge Update - BETA 4

--- ErrorReport/ErrorList.py
+++ ErrorReport/ErrorList.py
@@ -2,11 +2,11 @@
 ## This file contains events thats raised when the program must to stop
 
 def ImportLib():
-   raise RuntimeError(">> Could Not Import Library: Check if the libraries are installed and run the program again.")
+   raise RuntimeError(">> Could not import library: Check if the libraries are installed and run the program again.")
 
 def FileExists():
-   raise RuntimeError(">> The File Already Exists!")
+   raise RuntimeError(">> The file already exists!")
 
 def DirectoryExists():
-   raise RuntimeError(">> The Directory Already Exists!")
+   raise RuntimeError(">> The directory already exists!")
 
