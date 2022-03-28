3359d92a328cffa3351d3b068836e9aec9c9246b
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2022-03-28 08:35:30 -0300

    Added __main__.py

---
+++ __main__.py
@@ -0,0 +1,35 @@
+## PyBridge File
+## This file is used to first run your application
+## Here the contents will be processed to choose the best platform to go
+
+try:
+    ## Native Libraries
+    from sys import platform
+
+    ## Local Libraries
+    from exception import Exceptions
+
+except:
+    Separator = "="*80
+    Message = ">> Couldn`t Import Libraries:\n>> Check if the libraries are installed and run the program again."
+    raise RuntimeError("\n\n\n" + Separator + "\n" + Message + "\n" + Separator)
+
+def Main():
+    Platform = platform
+    
+    ## Linux
+    if Platform == "linux" or Platform == "linux2":
+        from Linux import Linux
+        Linux.Linux()
+
+    ## Mac    
+    elif Platform == "darwin":
+        from Mac import Mac
+        Mac.Mac()
+        
+    ## Windows
+    elif Platform == "win32" or Platform == "win64":
+        from Windows import Windows
+        Windows.Windows()
+
+Main()
