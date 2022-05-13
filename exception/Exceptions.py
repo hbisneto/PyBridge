4d5cfa36e473e03c72fd6339a1a69d2206288928
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2022-05-13 17:22:45 -0300

    Exceptions
    Updated Exceptions Library

--- exception/Exceptions.py
+++ exception/Exceptions.py
@@ -1,10 +1,11 @@
-﻿## ErrorList File
+## ErrorList File
 ## This file contains events that's raised when the program must to stop
 
-class Raise():
-   class Requirements():
+class Raise:
+  class Requirements():
       def MajorVersion(self, CurrentVersion, TargetVersion, TargetMajor):
-         raise Exception(f'\n\n>> You cannot run the application because it requires Python {TargetVersion} or later. [Current Version: {CurrentVersion}]')
+         raise Exception(
+             f'\n\n>> You cannot run the application because it requires Python {TargetVersion} or later. [Current Version: {CurrentVersion}]')
 
       def MinorVersion(self, CurrentVersion, TargetVersion, TargetMinor):
          print("="*40)
@@ -23,44 +24,55 @@ class Raise():
       def BuildVersion(self, CurrentVersion, TargetVersion, BuildVer):
          raise Exception(f'>> This application only can run on Python {TargetVersion}. [Current Version: {CurrentVersion}]')
 
-def ImportLib():
-   raise RuntimeError("\n\n\n>> Could not import library: Check if the libraries are installed and run the program again.")
 
-def FileExists():
-   raise RuntimeError("\n\n\n>> The file already exists!")
+  def __init__(self, exctype):
+    self.exctype = exctype
 
-def DirectoryExists():
-   raise RuntimeError("\n\n\n>> The directory already exists!")
+  def FileExists(self):
+    raise Exception(f'{self.exctype} The file already exists')
+  
+  def DirectoryExists(self):
+    raise Exception(f'{self.exctype} The directory already exists')
+  
+  def ImportLib():
+   raise RuntimeError(">> Could not import library: Check if the libraries are installed and run the program again.")
 
-def InputFormat():
-   print("\n\n\n>> Your input is not valid: Check your input and try again\n\n")
+  def InputFormat():
+    print(">> Your input is not valid: Check your input and try again\n\n")
 
-def InvalidOption():
-   print("="*80)
-   print(">> Invalid Option!")
-   print("="*80)
-   print(f'>> You typed an invalid option.\n>> Running the program again!')
-   print("="*80)
+  def InvalidOption():
+    print("=" * 80)
+    print(">> Invalid Option!")
+    print("=" * 80)
+    print(f'>> You typed an invalid option.\n>> Running the program again!')
+    print("=" * 80)
 
-def ProjectsLoadFail():
-   print("="*80)
-   print(f'>> ERROR: Couldn`t load projects...')
-   print("="*80)
+  def ProjectsLoadFail():
+    print("=" * 80)
+    print(f'>> ERROR: Couldn`t load projects...')
+    print("=" * 80)
 
-def BackupFail():
-   print("="*80)
-   print(">> BACKUP CREATION FAILED!")
-   print("="*80)
-   print("[=]" * 20)
-   print(f'>> PyBridge could not create backup for your projects folder')
-   print(f'>> Try again later.')
-   print("[=]" * 20)
+  def BackupFail():
+    print("=" * 80)
+    print(">> BACKUP CREATION FAILED!")
+    print("=" * 80)
+    print("[=]" * 20)
+    print(f'>> PyBridge could not create backup for your projects folder')
+    print(f'>> Try again later.')
+    print("[=]" * 20)
 
-def CompressBackupFail():
-   print("="*80)
-   print(">> COMPRESSED FILE CREATION FAILED!")
-   print("="*80)
-   print("[=]" * 20)
-   print(f'>> PyBridge could not create a compressed file from your backup')
-   print(f'>> Try again later.')
-   print("[=]" * 20)
+  def CompressBackupFail():
+    print("=" * 80)
+    print(">> COMPRESSED FILE CREATION FAILED!")
+    print("=" * 80)
+    print("[=]" * 20)
+    print(f'>> PyBridge could not create a compressed file from your backup')
+    print(f'>> Try again later.')
+    print("[=]" * 20)
+
+Throw = Raise("\n\n\n>> An Exception occurred:")
+
+# Throw.FileExists()
+# Throw.DirectoryExists()
+
+#Exceptions.Throw.FileExists()
